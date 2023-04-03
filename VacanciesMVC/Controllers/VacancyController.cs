fce27a51510a85cdaee75ced21d174e3b938f49b
staskukhar <108075412+staskukhar@users.noreply.github.com>
2023-04-03 21:55:48 +0300

    Add project files.

---
+++ VacanciesMVC/Controllers/VacancyController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TasteWork;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
+using VacanciesMVC.Models;
+
+namespace VacanciesMVC.Controllers
+{
+    public class VacancyController : Controller
+    {
+        MyDbContext db;
+        public VacancyController(MyDbContext dependencyInjection)
+        {
+            db = dependencyInjection;
+        }
+
+        public IActionResult Category()
+        {
+            IEnumerable<string> category = db.Vacancy
+                .Select(v => v.TypeVacancy)
+                .Distinct();
+            return View(category);
+        }
+        public async Task<IActionResult> Index(string type, int page = 1)
+        {
+            var filteredVacancy = db.Vacancy.Where(v => v.TypeVacancy == type);
+
+            ViewData["type"] = type;
+            return View(await PaginatedList<Vacancy>.CreateAsync(filteredVacancy, page, 10));
+        }
+    }
+}
